39b2d192bf13a8e2734faccb4e1a49bce16c2e28
David Castro Cisneros <david.castro.cisneros@gmail.com>
2024-09-09 12:51:07 -0300

    guarantee that empty state is localized

--- GradientListFeature/Presentation/GroceryListViewModel.swift
+++ GradientListFeature/Presentation/GroceryListViewModel.swift
@@ -8,10 +8,15 @@
 final class GroceryListViewModel {
     private enum LocalizableKey: String.LocalizationValue {
         case title = "GROCERY_LIST_TITLE"
+        case emptyStateMessage = "GROCERY_LIST_NO_ITEM_FOUND"
     }
     
     var title: String {
         String(localizedKey: LocalizableKey.title.rawValue)
+    }
+    
+    var emptyStateMessage: String {
+        String(localizedKey: LocalizableKey.emptyStateMessage.rawValue)
     }
     
     init() {
--- GradientListFeatureTests/GradientListViewModelTests.swift
+++ GradientListFeatureTests/GradientListViewModelTests.swift
@@ -12,4 +12,8 @@ final class GroceryListViewModelTests: XCTestCase {
     func test_title_isLocalized() {
         XCTAssertEqual(GroceryListViewModel().title, NSLocalizedString("GROCERY_LIST_TITLE", tableName: "GroceriesListFeature", comment: "Verifying key existance"))
     }
+    
+    func test_emptyMessage_isLocalized() {
+        XCTAssertEqual(GroceryListViewModel().emptyStateMessage, NSLocalizedString("GROCERY_LIST_NO_ITEM_FOUND", tableName: "GroceriesListFeature", comment: "Verifying key existance"))
+    }
 }
