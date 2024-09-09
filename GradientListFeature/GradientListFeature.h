75afdc6fd4e869fccd936267a2644426e921ea27
David Castro Cisneros <david.castro.cisneros@gmail.com>
2024-09-09 10:43:13 -0300

    Add initial project setup

---
+++ GradientListFeature/GradientListFeature.h
@@ -0,0 +1,18 @@
+//
+//  GradientListFeature.h
+//  GradientListFeature
+//
+//  Created by David Castro Cisneros on 09/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GradientListFeature.
+FOUNDATION_EXPORT double GradientListFeatureVersionNumber;
+
+//! Project version string for GradientListFeature.
+FOUNDATION_EXPORT const unsigned char GradientListFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientListFeature/PublicHeader.h>
+
+
---
+++ Gradients/GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientsApp.swift
+//  Gradients
+//
+//  Created by David Castro Cisneros on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
